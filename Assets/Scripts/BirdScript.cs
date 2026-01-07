2145118154be72d86fc9f15e79021a2b4b6f5214
Muhammad Fairooz <fairooz2150@gmail.com>
2026-01-07 15:26:37 +0530

    Game over panel setup

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,20 +6,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
-
+    public ScoreManager score;
+    public bool birdIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreManager>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
 
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        score.showGameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int playerScore;
     public TMP_Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int point)
     {
         playerScore = playerScore + point;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void showGameOver()
+    {
+     gameOverScreen.SetActive(true);    
     }
 }
