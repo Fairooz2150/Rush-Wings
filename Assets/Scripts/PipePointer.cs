ae6026f4cc025e0c14456a333cdcd99f2e8860bf
Muhammad Fairooz <fairooz2150@gmail.com>
2026-02-03 18:24:28 +0530

    Pipe pointer script created

---
+++ Assets/Scripts/PipePointer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePointer : MonoBehaviour
+{
+
+    public Transform bottomPipe;
+    public float moveAmount = 12.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Arrow")
+        {
+           Vector3 pos = bottomPipe.position;
+           pos.y -= moveAmount;
+           bottomPipe.position = pos;
+        }
+    }
+}
