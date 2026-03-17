2ec8b7033ed0b8ee0f407e97bac6acb458ead326
Muhammad Fairooz <fairooz2150@gmail.com>
2026-03-17 11:48:32 +0530

    Created a cooldown time to shoot

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 
 public class ShootingScript : MonoBehaviour
 {
-    [SerializeField]
-    GameObject arrowPrefab;
+    [SerializeField] GameObject arrowPrefab;
 
-    [SerializeField]
-    Transform arrowPos;
+    [SerializeField] Transform arrowPos;
 
-    [SerializeField]
-    Transform bird;
+    [SerializeField] Transform bird;
 
-    [SerializeField]
-    float shootForce = 15f;
+    [SerializeField] float shootForce = 15f;
 
+    [SerializeField] float cooldownTime = 2.5f; 
+
+    float currentCooldown = 0f;
+
+
+    void Update()
+    {
+        //reduce cooldown over time
+        if(currentCooldown > 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+    }
     public void Shoot()
     {
-        // Vector3 birdPos = bird.position;
-
-        // birdPos.y -= 2f;
+       // Block shooting if still cooling down
+       if (currentCooldown > 0)
+        return;
 
         GameObject arrow = Instantiate(
             arrowPrefab,
@@ -31,6 +40,9 @@ public class ShootingScript : MonoBehaviour
 
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
         rb.AddForce(bird.right * shootForce, ForceMode2D.Impulse);
+
+        //Reset cooldown
+        currentCooldown = cooldownTime;
     }
-        
+
 }
